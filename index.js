0040556adff12613b131af931e8467ba3b937fcc
German9105 <knvb2705@hotmail.com>
2022-07-05 21:34:51 -0500

    clase variables

---
+++ index.js
@@ -0,0 +1,30 @@
+// Comentarios de una sola linea
+/* Comentarios en muchas lineas */
+
+/*console.log("Hola Mundo!")
+
+// Outputs
+alert("Bienvenido al Imperio de la muerte")
+console.log("Hola", prompt("Estas seguro de querer continuar?"))
+document.write("Bienvenido a las Catacumbas de París!!")
+
+// Tipos de Datos primitivos
+//string "Soy un string"
+//numbers 1,2,3,4,
+//null 
+// undefinided
+//symbol - sirve para rehusar memoria
+
+// "Datos de prototipo"
+//objetc {}
+// array []*/
+
+//Variable decretada pero sin valor
+var variableUno;
+variableUno = "Cambie de valor"
+//Se asigna valor
+var variableDos = "German";
+
+
+
+
---
+++ variables.js
@@ -0,0 +1,23 @@
+var pizza;
+console.log("line 2 - pizza", pizza);
+
+var taco = "Carnitas"
+console.log("line 6 - taco", taco);
+
+pizza = "peperoni";
+console.log("line 8 - pizza", pizza);
+var string = " Esto es un string";
+var numero = 8;
+var nulo = null;
+var undef = undefined;
+var bool = true;
+console.log("line 11 - string,numero,nulo,undef,bool",taco)
+console.log("line 12",8+8);
+
+var uno = 12;
+var dos = 13;
+console.log("line 13",uno+dos)
+
+var nombre = prompt("Cual es tu nombre");
+document.write("hola" +" "+ nombre )
+
